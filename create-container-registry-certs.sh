0228a80d8ad7de487572a4be35efdf1dd3147bce
Thorsten Kukuk <kukuk@suse.com>
2019-10-07 16:42:13 +0200

    Implment creating certificates for given or local hostnames

--- create-container-registry-certs.sh
+++ create-container-registry-certs.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 CERTDIR=/etc/registry/certs
 
-if [ ! -n "$*" ]; then
+LANG=C
+
+if [ -n "$*" ]; then
   HOSTNAMES="$@"
 else
   HOSTNAMES=`hostname -f`
 fi
 
 mkdir -p ${CERTDIR}
-certstrap --depot-path ${CERTDIR} init --common-name "ContainerRegistryCA" --passphrase ""
-certstrap --depot-path ${CERTDIR} request-cert -ip 192.168.107.90 -domain microos.demo --passphrase ""
-certstrap --depot-path ${CERTDIR} sign microos.demo --CA "ContainerRegistryCA"
-ln -sf ${CERTDIR}/ContainerRegistryCA.crt /etc/pki/trust/anchors/RegistryCA.pem
 
+IP_ADDRS=`getent ahosts ${HOSTNAMES} | awk '{print $1}' | sort -u`
+IP_ADDRS=`echo -n ${IP_ADDRS} | tr ' ' ','`
+HOSTNAMES=`echo -n ${HOSTNAMES} | tr ' ' ','`
+
+certstrap --depot-path ${CERTDIR} init --common-name "ContainerRegistryCA" --passphrase ""
+certstrap --depot-path ${CERTDIR} request-cert -ip ${IP_ADDRS} -domain ${HOSTNAMES} --passphrase "" --common-name registry
+certstrap --depot-path ${CERTDIR} sign registry --CA "ContainerRegistryCA"
+ln -sf ${CERTDIR}/ContainerRegistryCA.crt /etc/pki/trust/anchors/RegistryCA.pem
+update-ca-certificates
